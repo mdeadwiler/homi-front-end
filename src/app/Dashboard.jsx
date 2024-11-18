c4e4041538dbdd3aa643ed1c5f5411e1cc6d3f78
Rani Bungay <ranibungay@gmail.com>
2024-11-18 07:18:05 -0500

    clean up user dashboard

--- src/app/Dashboard.jsx
+++ src/app/Dashboard.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { MyUserInfo } from "../components/MyUserInfo";
 
 export const Dashboard = () => {
   return (
     <>
       <div>Dashboard</div>
-      <MyUserInfo />
     </>
   );
 };
